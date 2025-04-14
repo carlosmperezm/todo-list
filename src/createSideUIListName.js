8a463cc1bb3546e568d0666c26d95ad1b7dd575e
CarlosPerez <carlosmperez2901@gmail.com>
2025-04-14 00:10:32 -0700

    Name refactor and add atributes to use them to save the objects in localStorage

--- src/createSideUIListName.js
+++ src/createSideUIListName.js
@@ -2,20 +2,22 @@ import "./sideListName.css";
 
 export function createSideUIListName(todoList) {
   const listNameContainer = document.createElement("div");
-  const title = document.createElement("span");
+  const listName = document.createElement("span");
   const counterContainer = document.createElement("span");
   const counter = document.createElement("span");
 
   listNameContainer.classList.add("list-name-container")
+  listNameContainer.dataset.active = todoList.active;
+  listNameContainer.name = todoList.name;
 
-  title.textContent = todoList.name;
+  listName.textContent = todoList.name;
 
   counterContainer.classList.add("counter-container");
   counter.textContent = todoList.getAll().length;
 
   counterContainer.appendChild(counter);
 
-  listNameContainer.appendChild(title);
+  listNameContainer.appendChild(listName);
   listNameContainer.appendChild(counterContainer);
 
   return listNameContainer;
