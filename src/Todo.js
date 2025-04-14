fea0f25eab2d50f094cbba864b5da8576a462948
CarlosPerez <carlosmperez2901@gmail.com>
2025-04-14 00:05:37 -0700

    Add methods to create and expose JSON type objects for better compability with localStorage

--- src/Todo.js
+++ src/Todo.js
@@ -6,6 +6,16 @@ export class Todo {
   #priority;
   #isDone;
 
+
+  static fromJSON(jsonObject) {
+    const title = jsonObject.title;
+    const description = jsonObject.description;
+    const dueDate = jsonObject.dueDate;
+    const priority = jsonObject.piority;
+    const isDone = jsonObject.isDone;
+
+    return new Todo(title, description, dueDate, priority, isDone);
+  }
   constructor(title, dueDate, description = "", priority = "medium", isDone = false) {
     this.#title = title;
     this.#dueDate = dueDate;
@@ -42,6 +52,15 @@ export class Todo {
   }
   toggleDone() {
     this.#isDone ? this.#isDone = false : this.#isDone = true;
+  }
+  toJSON() {
+    return {
+      title: this.#title,
+      description: this.#description,
+      dueDate: this.#dueDate,
+      priority: this.#priority,
+      isDone: this.#isDone
+    };
   }
 }
 
