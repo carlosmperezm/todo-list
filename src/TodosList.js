6232f327c68b3f9c4dd379741aa29f326ee6eec2
CarlosPerez <carlosmperez2901@gmail.com>
2025-04-14 00:06:50 -0700

    Add methods to change active status to indetify the current lists and add method for JSON parsing

--- src/TodosList.js
+++ src/TodosList.js
@@ -1,9 +1,19 @@
 export class TodoList {
   #name;
+  #active = false;
   #todos = [];
 
-  constructor(name, todos = []) {
+  static fromJSON(jsonObject) {
+    const name = jsonObject.name;
+    const active = jsonObject.active;
+    const todos = jsonObject.todos;
+
+    return new TodoList(name, active, todos);
+  }
+
+  constructor(name, active = false, todos = []) {
     this.#name = name;
+    this.#active = active;
     this.#todos = todos;
   }
 
@@ -13,7 +23,13 @@ export class TodoList {
   set name(newName) {
     this.#name = newName;
   }
-
+  get active() {
+    return this.#active;
+  }
+  set active(newActiveState) {
+    if (typeof newActiveState === "boolean")
+      this.#active = newActiveState;
+  }
   add(newTodo) {
     this.#todos.push(newTodo);
   }
@@ -27,7 +43,12 @@ export class TodoList {
     return this.#todos;
   }
 
-
-
+  toJSON() {
+    return {
+      name: this.#name,
+      active: this.#active,
+      todos: this.#todos
+    };
+  }
 
 }
