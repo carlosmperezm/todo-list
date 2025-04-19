3e5d87db3146db9eefa288f0e21352931cc6b92b
CarlosPerez <carlosmperez2901@gmail.com>
2025-04-18 18:55:48 -0700

    Convert some regular operations into methods

--- src/ListStorageController.js
+++ src/ListStorageController.js
@@ -16,4 +16,38 @@ export class ListStorageController {
     const listJSON = JSON.parse(localStorage.getItem(name.toLowerCase()));
     return TodoList.fromJSON(listJSON).getAll();
   }
+
+  static toggleActive(listName) {
+    const listObj = ListStorageController.getList(listName);
+    const list = TodoList.fromJSON(listObj);
+    list.active = list.active ? false : true;
+    ListStorageController.saveList(list.name, list);
+  }
+
+  static setActiveList(listName) {
+    const listObj = ListStorageController.getList(listName);
+    const list = TodoList.fromJSON(listObj);
+    list.active = true;
+    ListStorageController.saveList(list.name, list);
+  }
+
+  static setInactiveList(listName) {
+    const listObj = ListStorageController.getList(listName);
+    const list = TodoList.fromJSON(listObj);
+    list.active = false;
+    ListStorageController.saveList(list.name, list);
+  }
+
+  static setInactiveAllListBut(listNameToKeepActive) {
+    for (let i = 0; i < localStorage.length; i++) {
+      const listName = localStorage.key(i);
+      const list = TodoList.fromJSON(ListStorageController.getList(listName));
+
+      if (list.name !== listNameToKeepActive) {
+        list.active = false;
+        ListStorageController.saveList(list.name, list);
+      }
+    }
+  }
+
 }
